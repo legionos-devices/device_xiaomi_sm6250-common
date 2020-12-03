507c269dfd481f330198cdc8d4d7cc61c2bf20c7
Rajkale99 <kaleraj.rk@gmail.com>
2020-12-03 17:32:15 +0000

    sm6250-common: add vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,4 @@
+rm -rf hardware/qcom-caf/sm8150
+git clone https://github.com/SonalSingh18/R-Hals.git -b display-8150 hardware/qcom-caf/sm8150/display
+git clone https://github.com/SonalSingh18/R-Hals.git -b audio-8150 hardware/qcom-caf/sm8150/audio
+git clone https://github.com/SonalSingh18/R-Hals.git -b media-8150 hardware/qcom-caf/sm8150/media
