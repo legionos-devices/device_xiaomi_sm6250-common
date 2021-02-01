59d92e4a3bd02602be5602583e7590dac367b345
rajkale99 <kaleraj.rk@gmail.com>
2021-02-01 13:13:33 +0530

    sm6250-common: Add vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,6 @@
+rm -rf hardware/qcom-caf/sm8150/display
+rm -rf hardware/qcom-caf/sm8150/media
+rm -rf hardware/qcom-caf/sm8150/audio
+git clone https://github.com/ArrowOS/android_hardware_qcom_display -b arrow-11.0-caf-sm8150 hardware/qcom-caf/sm8150/display
+git clone https://github.com/ArrowOS/android_hardware_qcom_media -b arrow-11.0-caf-sm8150 hardware/qcom-caf/sm8150/media
+git clone https://github.com/ArrowOS/android_hardware_qcom_audio -b arrow-11.0-caf-sm8150 hardware/qcom-caf/sm8150/audio
